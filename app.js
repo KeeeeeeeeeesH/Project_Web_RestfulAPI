09ffe21be1597d538dcb6a04f3d1e1c4f21cfbd8
champqazwsxedc <champ.za2002@hotmail.com>
2024-05-06 20:34:52 +0700

    add package CORE and index.html

--- app.js
+++ app.js
@@ -3,26 +3,28 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 const mysql = require('mysql2');
+const cors = require('cors');
 
-const pool = mysql.createPool({
-    host: 'localhost',  
-    user: 'root', 
-    password: 'newsroot123456',  
-    database: 'news_web_app',
-    port: 3306  
-});
-
-// set-up พรี่แชมป์
 // const pool = mysql.createPool({
 //     host: 'localhost',  
 //     user: 'root', 
-//     password: '',  
+//     password: 'newsroot123456',  
 //     database: 'news_web_app',
 //     port: 3306  
 // });
 
+// set-up พรี่แชมป์
+const pool = mysql.createPool({
+    host: 'localhost',  
+    user: 'root', 
+    password: '',  
+    database: 'news_web_app',
+    port: 3306  
+});
+
 module.exports = pool
 
+app.use(cors());
 app.use(express.json());
 
 app.use(bodyParser.json());
