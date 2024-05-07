b5bf762f70d01665141303223ff294311e072655
sirohmzz <sirohmzz@hotmail.com>
2024-05-07 18:45:22 +0700

    จัดหมวดหมู่หน้าบ้านใน views + อธิบายหน้าบ้าน

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/html', express.static(path.join(__dirname, 'html')));
+app.use('/views', express.static(path.join(__dirname, 'views/admin')));
 
 const AdminRouter = require('./routes/admin');
 app.use('/api/admin', AdminRouter);
