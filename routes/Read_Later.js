5585260db8847b015432a7774efb504ebb532b5b
champqazwsxedc <champ.za2002@hotmail.com>
2024-08-20 18:26:47 +0700

    ปรับไม่สามารถเเก้ข้อมูลสมาชิกได้

--- routes/Read_Later.js
+++ routes/Read_Later.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../app');
 
+router.get('/', (req, res) => {
+    pool.query('SELECT * FROM Read_Later', (error, results) => {
+        if (error) {
+            console.error('Error fetching Read Later: ', error);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        res.json(results);
+    });
+});
+
 router.get('/:memId', (req, res) => {
   const memId = req.params.memId;
   pool.query('SELECT * FROM Read_Later WHERE Mem_Id = ?', [memId], (error, results) => {
